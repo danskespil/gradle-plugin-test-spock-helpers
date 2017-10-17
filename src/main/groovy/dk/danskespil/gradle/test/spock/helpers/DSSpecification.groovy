ea740bc470415a4cf916417e4f4a8dca362f83b7
Jesper Wermuth <jwermuth@gmail.com>
2017-10-17 17:35:41 +0200

    Split out common test func into module
    The test-module gradle-test-spock-helpers is used to 2 other modules,
    which is actually kind of a test that it is working. Its turtles all the
    way down.

---
+++ src/main/groovy/dk/danskespil/gradle/test/spock/helpers/DSSpecification.groovy
@@ -0,0 +1,50 @@
+package dk.danskespil.gradle.test.spock.helpers
+
+import dk.danskespil.gradle.task.helpers.CommandLineExecutorFactory
+import org.junit.Rule
+import org.junit.rules.TemporaryFolder
+import spock.lang.Specification
+
+class DSSpecification extends Specification {
+    @Delegate
+    static TestHelper testHelper
+    @Rule
+    final TemporaryFolder testProjectDir = new TemporaryFolder()
+    File buildFile
+    PathMaker pathMaker = new PathMaker()
+
+    // run before the first feature method
+    def setupSpec() {
+        testHelper = new TestHelper()
+    }
+
+    // run before every feature method
+    def setup() {
+        buildFile = testProjectDir.newFile('build.gradle')
+        testHelper.testProjectDir = testProjectDir
+        setupTestStubsByAddingMarkerFilesToAConventionallyNamedDirectory()
+    }
+
+    // Easy creation of deep paths with or without files 'at the end'
+    File createNewPath(String path) {
+        pathMaker.createNewPath(testProjectDir, path)
+    }
+
+    private void setupTestStubsByAddingMarkerFilesToAConventionallyNamedDirectory() {
+        createNewPath("stubTheseFactories/${CommandLineExecutorFactory.getClass().getName()}")
+    }
+
+    boolean exists(String path) {
+        if (!path.startsWith(File.separator)) {
+            path += File.pathSeparator + path
+        }
+        return new File(testProjectDir.root.absolutePath + "${path}").exists()
+    }
+
+    File file(String path) {
+        if (!path.startsWith(File.separator)) {
+            path += File.pathSeparator + path
+        }
+        return new File(testProjectDir.root.getAbsolutePath() + path)
+    }
+}
