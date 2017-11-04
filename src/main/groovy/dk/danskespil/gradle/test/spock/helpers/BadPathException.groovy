3935fe149f9ff24dac1806368619ce7b2b89a6c6
Jesper Wermuth <jwermuth@gmail.com>
2017-11-04 15:29:32 +0100

    Correct substitution syntax. Fixes #45

--- src/main/groovy/dk/danskespil/gradle/test/spock/helpers/BadPathException.groovy
+++ src/main/groovy/dk/danskespil/gradle/test/spock/helpers/BadPathException.groovy
@@ -2,6 +2,6 @@ package dk.danskespil.gradle.test.spock.helpers
 
 class BadPathException extends RuntimeException{
     BadPathException(String badInput) {
-        super("You are trying to parse '#{badInput}' as a path. Hm. Does not work. Try something like 'path/to/something'")
+        super("You are trying to parse '${badInput}' as a path. Hm. Does not work. Try something like 'path/to/something'")
     }
 }
